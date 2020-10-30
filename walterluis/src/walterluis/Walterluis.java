029841fab60837b69aa345e37edfed0d2cdcdeb8
walter reyes <walter.reyes@cambridgehn.com>
2020-10-29 20:03:39 -0600

    inicio

--- walterluis/src/walterluis/Walterluis.java
+++ walterluis/src/walterluis/Walterluis.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package walterluis;
 
 /**
@@ -11,11 +7,10 @@ package walterluis;
  */
 public class Walterluis {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola mundo ");
+        int x = 0;
     }
     
 }
