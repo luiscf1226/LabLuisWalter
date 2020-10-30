23ac33533b96b08230e0f49b87f26cde521b7ba4
walter reyes <walter.reyes@cambridgehn.com>
2020-10-29 20:13:59 -0600

    alo

--- walterluis/src/walterluis/Walterluis.java
+++ walterluis/src/walterluis/Walterluis.java
@@ -12,6 +12,8 @@ public class Walterluis {
         System.out.println("Hola mundo ");
         int x = 0;
         System.out.println("Manana nos pisan jajajjaj");
+        System.out.println("Si el de arriba  lo permite");
+        System.out.println("Por la chingada, actualizate stronzo di merda");
     }
     
 }
